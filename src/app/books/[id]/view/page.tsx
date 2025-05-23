d6074c816ce83115beac7098ccd83e7c9d00143e
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-05-23 11:02:50 +0500

    Update page.tsx

--- src/app/books/[id]/view/page.tsx
+++ src/app/books/[id]/view/page.tsx
@@ -1,7 +1,7 @@
 import BookDetails from "@/components/BookDetails";
 import { ErrorBoundary } from "react-error-boundary";
-import { Metadata } from "next";
-import { api } from "@/utils/api";
+// import { Metadata } from "next";
+// import { api } from "@/utils/api";
 
 interface Props {
   params: Promise<{
@@ -9,28 +9,28 @@ interface Props {
   }>;
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // read route params
-  const { id } = await params;
-  try {
-    const book = await api.getBook(parseInt(id));
-    return {
-      title: `${book.title} by ${book.author} | ReadStack`,
-      description:
-        book.description || `View details for ${book.title} by ${book.author}`,
-      openGraph: {
-        title: book.title,
-        description: book.description,
-        images: [book.cover_image || ""],
-      },
-    };
-  } catch {
-    return {
-      title: "Book Details | ReadStack",
-      description: "View book details",
-    };
-  }
-}
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   // read route params
+//   const { id } = await params;
+//   try {
+//     const book = await api.getBook(parseInt(id));
+//     return {
+//       title: `${book.title} by ${book.author} | ReadStack`,
+//       description:
+//         book.description || `View details for ${book.title} by ${book.author}`,
+//       openGraph: {
+//         title: book.title,
+//         description: book.description,
+//         images: [book.cover_image || ""],
+//       },
+//     };
+//   } catch {
+//     return {
+//       title: "Book Details | ReadStack",
+//       description: "View book details",
+//     };
+//   }
+// }
 
 export default async function Page({ params }: Props) {
   const { id } = await params;
